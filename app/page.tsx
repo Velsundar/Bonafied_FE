9a7cf14c7b27dcc02ac4c839d559f0072b8e2226
Sundaravel <sundaravel.exe@gmail.com>
2024-01-27 09:14:16 +0530

    favicon, title,homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
+import Login from "./Login/page";
 
 export default function Home() {
   return (
-    <>
-      <h1>hi Indrajith</h1>
-      <h2>hi ragabvan</h2>
+      <>
+      <Login />
       </>
   );
 }
