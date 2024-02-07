223b365a1aa3e483b06f0d0dfe29c448bc882fcd
Sundaravel <sundaravel.exe@gmail.com>
2024-02-07 17:05:42 +0530

    admin unauthorized access UI

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -3,6 +3,7 @@ import NavBar from '../../../app/dashboard/navbar'
 import { Box } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
+import UnAutorizedAccess from '../../assets/png/Unauthorized_access.jpeg'
 
 const Admindash = () => {
   const history = useRouter();
@@ -15,9 +16,26 @@ const Admindash = () => {
 
   const token = localStorage.getItem("token");
   if (!token) {
-    return null; // or <LoadingIndicator />
+    return (
+      <div
+        style={{
+          backgroundImage: `url(${UnAutorizedAccess})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100%',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: 'white',
+          fontSize: '24px',
+          textAlign: 'center',
+        }}
+      >
+        Unauthorized - Please log in
+      </div>
+    );
   }
-  
   return (
     <Box>
       <NavBar/>
